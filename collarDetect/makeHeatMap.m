94bd019acdf8c7209583207f75b43ed19f30efc6
ntlhui <ntlhui@gmail.com>
2015-04-16 01:54:19 -0700

    Added UTM functionality

--- collarDetect/makeHeatMap.m
+++ collarDetect/makeHeatMap.m
@@ -1,6 +1,13 @@
 function [] = makeHeatMap(lat, lon, col)
 colormap(jet);
-scatter(lat, lon, 16, col);
+x = zeros(length(lat));
+y = zeros(length(lat));
+for i = 1:1:length(lat)
+    [xu,yu] = deg2utm(lat(i), lon(i));
+    x(i) = xu;
+    y(i) = yu;
+end
+scatter(x, y, 16, col);
 axis equal;
 colorbar
 end
