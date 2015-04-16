2c4cb9c959a9f485c7e0897c1f0639bd1a7a5d6f
ntlhui <ntlhui@gmail.com>
2015-04-16 01:52:35 -0700

    updated script to work

--- autostart/parser.sh
+++ autostart/parser.sh
@@ -3,36 +3,45 @@ if [ ! -e /sys/class/gpio/gpio60 ]
     then
     echo 60 > /sys/class/gpio/export
 fi
-while true; do
-	read -t 5 line
-    echo $line
+run=true
+while $run; do
+	sleep 0.5
+	read line
 	retval=$?
-	if [ "$retval" = "142" ]
-        then	# TIMEOUT!
-        echo "Error! TIMEOUT!"
-		echo high > /sys/class/gpio/gpio60/direction
-		exit 0
-		#reboot
-	fi
 	if [ "$retval" = "1" ]
         then	# EOF reached.  Normal end
-		echo high > /sys/class/gpio/gpio60/direction
-		exit 0
+        # echo "Found EOF, waiting..."
+        sleep 2
+        read line
+        retval=$?
+        if [ "$retval" = "1" ]
+        	then	# EOF reached, no change for 2 seconds!
+	        echo "Error! TIMEOUT! REBOOT!"
+			echo low > /sys/class/gpio/gpio60/direction
+			run=false;
+			killall collarTracker -s INT
+			# reboot
+		fi
 	fi
 	if [ "$retval" = "0" ]
+		echo $line
         then
-		if [ "$(echo $line | grep FILE | wc -l)" != "0" ]
+		if [ "$(echo $line | grep Frame | wc -l)" != "0" ]
             then
-			echo low > /sys/class/gpio/gpio60/direction
+            # echo "Got Frame!"
+			echo high > /sys/class/gpio/gpio60/direction
 		fi
 		if [ "$(echo $line | grep SIGINT | wc -l)" = "1" ]
             then
-			echo high > /sys/class/gpio/gpio60/direction
-			exit 0
+            # echo "Got SIGINT!, exiting..."
+			echo low > /sys/class/gpio/gpio60/direction
+			run=false;
 		fi
 		if [ "$(echo $line | grep GPS | wc -l)" = "1" ]
             then
-			echo low > /sys/class/gpio/gpio60/direction
+            # echo "Got GPS!"
+			echo high > /sys/class/gpio/gpio60/direction
 		fi
 	fi
 done
+echo low > /sys/class/gpio/gpio60/direction
