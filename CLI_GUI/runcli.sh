5dcc52df7f31c3223a6d6757ec31d30133d28f44
ntlhui <ntlhui@gmail.com>
2015-04-24 10:47:50 -0700

    Added run scripts from old git commit

--- CLI_GUI/runcli.sh
+++ CLI_GUI/runcli.sh
@@ -1,16 +1,11 @@
 #! /usr/bin/env bash
-read -e -p "Raw data path: "
-echo $REPLY > JOB
-read -p "Run number: "
-echo "curr_run: $REPLY" >> JOB
-echo "freq_em: 1000" >> JOB
-echo "pulse_ms: 20" >> JOB
-echo "lin_scale: 1" >> JOB
-echo "map_d: 128" >> JOB
-echo "alpha_c_thres: 500" >> JOB
-read -p "Number of collars: "
-echo "num_col: $REPLY" >> JOB
-echo "f_drift: -3000" >> JOB
+read -p "Enter run number: " run
+read -p "Enter flight altitude: " alt
+read -e -p "Enter Raw Data directory: " dir
+dir=$(echo $dir | sed "s%~%$HOME%")
+if [[ -w JOB ]]; then
+	rm JOB
+fi
 if [ -e "COL" ]
 then
 	echo "Collar definitions found!"
@@ -18,16 +13,33 @@ else
 	echo "Collar definitions not found!"
 	exit 1
 fi
+num_col=$(cat COL | wc -l)
+echo $dir"/" >> JOB
+echo "curr_run: "$run >> JOB
+echo "freq_em: 1000" >> JOB
+echo "pulse_ms: 20" >> JOB
+echo "lin_scale: 1" >> JOB
+echo "map_d: 128" >> JOB
+echo "alpha_c_thres: 500" >> JOB
+echo "num_col: "$num_col >> JOB
+echo "f_drift: -3000" >> JOB
 if [ -e "spectrumAnalysis" ]
 then
-	./spectrumAnalysis
+	./spectrumAnalysis > /dev/null
 else
 	if [ -e "build-spectrumAnalysis" ]
 	then
 		./build-spectrumAnalysis
-		./spectrumAnalysis
+		./spectrumAnalysis > /dev/null
 	else
 		echo "Could not find ./spectrumAnalysis!"
 	fi
 fi
+runFile=$(ls | grep -E RUN_[[:digit:]]\+$run.csv$)
+./altFilter.py $alt $runFile
+./finalAnalysis > /dev/null
+metaFile=$(ls | grep -E META_[[:digit:]]\+$run.csv)
+./spectraCollarID $num_col $runFile $metaFile
+
+
 
