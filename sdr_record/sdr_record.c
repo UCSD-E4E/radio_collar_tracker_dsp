9702262fcc2006b53350bee0748239a929c169df
ntlhui <ntlhui@gmail.com>
2015-07-31 16:44:51 -0700

    Fixed all problems, basic functionality works

--- sdr_record/sdr_record.c
+++ sdr_record/sdr_record.c
@@ -1,121 +1,257 @@
 /**
- * rtl-sdr, turns your Realtek RTL2832 based DVB dongle into a SDR receiver
- * Copyright (C) 2012 by Steve Markgraf <steve@steve-m.de>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * This program records broadband signal data from an RTL2832 based SDR to
+ * assist detection of radio collars.
+ * Copyright (C) 2015 by Nathan Hui <nthui@eng.ucsd.edu>
  */
-
-#include <signal.h>
-#include <rtl-sdr.h>
-#include <stdio.h>
 #include <unistd.h>
-#include <getopt.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <rtl-sdr.h>
+#include <signal.h>
+#include <pthread.h>
+#include <sys/queue.h>
+#include <time.h>
+#include <stdbool.h>
+#include "queue.h"
 
-#define SAMP_RATE 2048000
-#define CENTR_FREQ 172464000
+// Global constants
+#define FILE_CAPTURE_DAEMON_SLEEP_PERIOD_MS	500
 
-static int do_exit = 0;
-static rtlsdr_dev_t *dev = NULL;
+// Typedefs
+struct proc_queue_args{
+	int run_num;
+	int frame_len;
+};
 
-static void sighandler(int signum){
-	fprintf(stderr, "Signal caught, exiting!\n");
-	do_exit = 1;
-	rtlsdr_cancel_async(dev);
-}
+// Global variables
+rtlsdr_dev_t *dev = NULL;
+int run = 1;
+pthread_mutex_t lock;
+queue data_queue;
+int counter = 0;
 
-int main(int argc, char **argv){
+// Function Prototypes
+void sighandler(int signal);
+int main(int argc, char** argv);
+void* proc_queue(void* args);
+static void rtlsdr_callback(unsigned char* buf, uint32_t len, void *ctx);
 
-	// Setting up variables
-	struct sigaction sigact;
-	int gain = 0;
+/**
+ * Main function of this program.
+ */
+int main(int argc, char** argv){
+	// Set up varaibles
+	// Options variable
 	int opt;
-	FILE* file;
+	// SDR Gain
+	int gain = 0;
+	// Sampling Frequency for SDR in samples per second
+	int samp_freq = 2048000;
+	// Center Frequency for SDR in Hz
+	int center_freq = 172464000;
+	// Nominal pulse length in ms
+	int pulse_length = 0;
+	// Nominal pulse period in m
+	int pulse_per = 1500;
+	// Run number
+	int run_num = -1;
+	// Block size
+	int block_size = 0;
+	// Start Time
+	struct timespec start_time;
+	// End Time
+	struct timespec end_time;
+	// Thread attributes
+	pthread_attr_t attr;
+	// Thread ID
+	pthread_t thread_id;
+	// proc_queue args
+	struct proc_queue_args pargs;
 
-	// Getting command line options
-	while ((opt = getopt(argc, argv, "g")) != -1){
+	// Get command line options
+	printf("Getting command line options\n");
+	while ((opt = getopt(argc, argv, "g:s:f:p:r:P:")) != -1){
 		switch(opt){
 			case 'g':
 				gain = (int)(atof(optarg) * 10);
 				break;
+			case 's':
+				samp_freq = (int)(atof(optarg));
+				break;
+			case 'f':
+				center_freq = (int)(atof(optarg));
+				break;
+			case 'p':
+				pulse_length = (int)(atof(optarg));
+				break;
+			case 'r':
+				run_num = (int)(atof(optarg));
+				break;
+			case 'P':
+				pulse_per = (int)(atof(pulse_per));
+				break;
 		}
 	}
+	if(run_num == -1){
+		// TODO: add usage notification here!
+		fprintf(stderr, "Bad RUN number!\n");
+		exit(-1);
+	}
+	printf("done\n");
 
-	// Opening SDR
+	// Initialize environment
+	printf("Configuring environment\n");
+	block_size = (int)(pulse_per / 1000.0 * 2 * samp_freq);
+	block_size = 262144;
+	queue_init(&data_queue);
+	printf("Done configuring environment\n");
+
+	// Open SDR
+	printf("Opening SDR\n");
 	if(rtlsdr_open(&dev, 0)){
-		fprintf(stderr, "Failed to open rtlsdr device!");
+		fprintf(stderr, "Failed to open rtlsdr device\n");
 		exit(1);
 	}
+	printf("SDR Opened\n");
 
-	// Setting signal handlers
-	sigact.sa_handler = sighandler;
-	sigemptyset(&sigact.sa_mask);
-	sigact.sa_flags = 0;
-	sigaction(SIGINT, &sigact, NULL);
-	sigaction(SIGTERM, &sigact, NULL);
-	sigaction(SIGQUIT, &sigact, NULL);
-	sigaction(SIGPIPE, &sigact, NULL);
-
-	// Setting sample rate
-	if(rtlsdr_set_sample_rate(dev, SAMP_RATE)){
-		fprintf(stderr, "WARNING: Failed to set sample rate.\n");
-		exit(1);
-	}
-	// Setting center frequency
-	if(rtlsdr_set_center_freq(dev, CENTR_FREQ)){
-		fprintf(stderr, "WARNING: Failed to set center freq.\n");
-		exit(1);
-	}
-
-	// Setting gain
+	// Configure SDR
+	printf("Configuring SDR\n");
 	if(gain == 0){
 		if(rtlsdr_set_tuner_gain_mode(dev, 0)){
-			fprintf(stderr, "WARNING: Failed to set tuner gain.\n");
+			fprintf(stderr, "ERROR: Failed to set tuner gain.\n");
 			exit(1);
 		}
 	}else{
 		if(rtlsdr_set_tuner_gain_mode(dev, 1)){
-			fprintf(stderr, "WARNING: Failed to enable manual gain.\n");
+			fprintf(stderr, "ERROR: Failed to enable manual gain.\n");
 			exit(1);
 		}
 		if(rtlsdr_set_tuner_gain(dev, gain)){
-			fprintf(stderr, "WARNING: Failed to set tuner gain.\n");
+			fprintf(stderr, "ERROR: Failed to set tuner gain.\n");
 			exit(1);
 		}
 	}
-
-	// Setting output filename
-	// FIXME: Need smarter filename
-	file = fopen("tmp", "wb");
-	if(!file){
-		fprintf(stderr, "Failed to open file!\n");
-	}
-
-	// Reset endpoint before we start reading from it (mandatory)
-	if(rtlsdr_reset_buffer(dev)){
-		fprintf(stderr, "WARNING: Failed to reset buffers\n");
+	if(rtlsdr_set_center_freq(dev, center_freq)){
+		fprintf(stderr, "ERROR: Failed to set center frequency.\n");
 		exit(1);
 	}
+	if(rtlsdr_set_sample_rate(dev, samp_freq)){
+		fprintf(stderr, "ERROR: Failed to set sampling frequency.\n");
+		exit(1);
+	}
+	if(rtlsdr_reset_buffer(dev)){
+		fprintf(stderr, "ERROR: Failed to reset buffers.\n");
+		exit(1);
+	}
+	printf("SDR Configured.\n");
 
-	// FIXME: Add timestamp here!
-	// Read samples in async mode
-	int r = rtlsdr_read_async(dev, rtlsdr_callback, (void*) file, 0, out_block_size);
-	/*
-	 * TODO: Set up callback to copy frame to worker thread's memory space, 
-	 * have worker send data to file.
-	 */
+	// Configure signal handlers
+	printf("Configuring signal handlers.\n");
+	signal(SIGINT, sighandler);
+	signal(SIGTERM, sighandler);
+	printf("Signal handlers configured\n");
 
-	fclose(file);
+	// Configure worker thread
+	printf("Configuring worker thread\n");
+	if(pthread_attr_init(&attr)){
+		fprintf(stderr, "ERROR: Failed to create default thread attributes.\n");
+		exit(1);
+	}
+	if(pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED)){
+		fprintf(stderr, "ERROR: Failed to create detached thread.\n");
+		exit(1);
+	}
+	pargs.run_num = run_num;
+	pargs.frame_len = block_size;
+	if(pthread_create(&thread_id, &attr, proc_queue, (void*) &pargs)){
+		fprintf(stderr, "ERROR: Failed to create detached thread.\n");
+		exit(1);
+	}
+	printf("Worker thread configured\n");
+
+	// Get timestamp
+	printf("Starting record\n");
+	clock_gettime(CLOCK_REALTIME, &start_time);
+	// begin recording
+	rtlsdr_read_async(dev, rtlsdr_callback, (void*) &data_queue, 0, block_size);
+	// clean up
+	clock_gettime(CLOCK_REALTIME, &end_time);
+	printf("Stopping record\n");
 	rtlsdr_close(dev);
+}
+
+/**
+ * Signal handler for this program.  Cancels the asynchronous read for the SDR
+ * and notifies all threads to finish execution.
+ */
+void sighandler(int signal){
+	printf("Signal caught, exiting\n");
+	run = 0;
+	rtlsdr_cancel_async(dev);
+}
+
+/**
+ * Worker thread function.  Reads information from the queue and writes it to
+ * the disk.
+ */
+void* proc_queue(void* args){
+	struct proc_queue_args* pargs = (struct proc_queue_args*) args;
+	int run_num = pargs->run_num;
+	int frame_len = pargs->frame_len;
+	FILE* data_stream;
+	char buff[256];
+	int frame_num;
+
+	frame_num = 0;
+	// Lock mutex
+	pthread_mutex_lock(&lock);
+	bool empty = true;
+	empty = queue_isEmpty(&data_queue);
+	pthread_mutex_unlock(&lock);
+
+	printf("Running thread\n");
+
+	while(run || !empty){
+		printf("Checking queue\n");
+		pthread_mutex_lock(&lock);
+		empty = queue_isEmpty(&data_queue);
+		pthread_mutex_unlock(&lock);
+
+		if(!empty){
+			// Process queue
+			printf("Got frame\n");
+			snprintf(buff, sizeof(buff),
+					"RAW_DATA_%06d_%06d", run_num,
+					frame_num %4 + 1);
+			data_stream = fopen(buff, "ab");
+			pthread_mutex_lock(&lock);
+			char* data_ptr = NULL;
+			data_ptr = (char*) queue_pop(&data_queue);
+			pthread_mutex_unlock(&lock);
+
+			printf("Writing frame\n");
+			fwrite(data_ptr, sizeof(char), frame_len, data_stream);
+			fclose(data_stream);
+
+			free(data_ptr);
+			frame_num++;
+			printf("done\n");
+		}else{
+			printf("No frame\n");
+			usleep(FILE_CAPTURE_DAEMON_SLEEP_PERIOD_MS * 1000);
+		}
+	}
+	return NULL;
+}
+
+static void rtlsdr_callback(unsigned char* buf, uint32_t len, void *ctx){
+	if(!run){
+		return;
+	}
+	char* newframe = malloc(len * sizeof(char));
+	memcpy(newframe, buf, len);
+	pthread_mutex_lock(&lock);
+	queue_push((queue*)ctx, (void*) newframe);
+	pthread_mutex_unlock(&lock);
 }
