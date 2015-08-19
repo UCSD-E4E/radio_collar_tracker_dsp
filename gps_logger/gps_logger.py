2d048eb23a0cedda719555a5075b2b0f4cd0f0d2
ntlhui <ntlhui@gmail.com>
2015-08-19 11:00:42 -0700

    Made changes to accept command line arguments.  Note that this version will be the last using MAVProxy due to instability in MAVProxy and DroneAPI

--- gps_logger/gps_logger.py
+++ gps_logger/gps_logger.py
@@ -10,31 +10,23 @@ from droneapi.lib import VehicleMode
 from pymavlink import mavutil
 import time
 import argparse
-import signal
-
-parser = argparse.ArgumentParser();
-parser.add_argument("-d", dest="destination", default="/media/RAW_DATA/rct/")
-parser.add_argument("-p", dest="prefix", default="GPS_")
-parser.add_argument("-s", dest="suffix", default="")
-parser.add_argument("-r", dest="runNum", default=1)
 
 argfile = open("gps_logger_args", "r")
+dataDir = argfile.readline().strip()
+gpsPrefix = argfile.readline().strip()
+gpsSuffix = argfile.readline().strip()
+runNum = int(argfile.readline().strip())
 
-args = parser.parse_args(argfile.readline().strip().split())
-dataDir = args.destination
-gpsPrefix = args.prefix
-gpsSuffix = args.suffix
-runNum = int(args.runNum)
 
 api = local_connect()
 v = api.get_vehicles()[0]
 
-logfile = open("%s/%s%d%s" % (dataDir, gpsPrefix, runNum, gpsSuffix), "w")
+logfile = open("%s/%s%d" % (dataDir, gpsPrefix, runNum), "w")
 
-current_milli_time = lambda: int(round(time.time() * 1000))
+print "Running"
 
 while True:
     if v.location.lat is not None and v.location.lon is not None:
-    	logfile.write("%.3f, %f, %f\n" % (current_milli_time() / 1000.0, float(v.location.lat), float(v.location.lon)))
+    	logfile.write("%.3f, %f, %f\n" % (round(time.time()) / 1000.0, float(v.location.lat), float(v.location.lon)))
     time.sleep(0.5)
 
