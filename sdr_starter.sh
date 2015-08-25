7f1e00366c3200d7eef4e68ff054b958656eee75
ntlhui <ntlhui@gmail.com>
2015-08-24 22:54:47 -0700

    Merged functionality of parser into sdr_starter.sh

--- sdr_starter.sh
+++ sdr_starter.sh
@@ -6,6 +6,14 @@ gain="19.7"
 output="/home/pi/rct/"
 sampling_freq=2048000
 port="/dev/ttyAMA0"
+led_num=17
+
+
+led_dir="/sys/class/gpio/gpio$led_num"
+if [ ! -e $led_dir ]
+then
+	echo $led_num > /sys/class/gpio/export
+fi
 
 while getopts "r:f:g:o:s:p:" opt; do
 	case $opt in
@@ -60,10 +68,21 @@ mavproxypid=$!
 /home/pi/radio_collar_tracker/sdr_record/sdr_record -g $gain -s $sampling_freq -f $freq -r $run -o $output &
 sdr_record_pid=$!
 
-/home/pi/radio_collar_tracker/autostart/parser.sh $mavproxypid $sdr_record_pid
-parser_pid=$!
-trap "echo 'got sigint'; /bin/kill -s SIGINT $mavproxypid; /bin/kill -s SIGINT $sdr_record_pid; /bin/kill -s SIGINT $parser_pid; sleep 1; rm gps_logger_args; exit 0" SIGINT SIGTERM
-while :
+trap "echo 'got sigint'; /bin/kill -s SIGINT $mavproxypid; /bin/kill -s SIGINT $sdr_record_pid; echo low > $led_dir/direction; sleep 1; rm gps_logger_args; exit 0" SIGINT SIGTERM
+run=true
+while $run:
 do
 	sleep 1
+	echo high > $led_dir/direction
+	if ! ps -p $mavproxypid > /dev/null
+	then
+		run=false
+	fi
+	if ! ps -p $sdr_record_pid > /dev/null
+	then
+		run=false
+	fi
 done
+echo low > $led_dir/direction
+/bin/kill -s SIGINT $mavproxypid
+/bin/kill -s SIGINT $sdr_record_pid
