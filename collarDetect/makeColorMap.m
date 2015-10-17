243473f9f7ed63f01fc3bcbac70acf37df17bca8
ntlhui <ntlhui@gmail.com>
2015-10-17 11:19:37 -0700

    Removed post-processing code from payload branch

--- collarDetect/makeColorMap.m
+++
@@ -1,9 +0,0 @@
-function [img] = makeColorMap(lat, lon, col, noise, scale)
-img = ones(floor((max(lon) - min(lon))/scale) + 1, floor((max(lat) - min(lat))/scale) + 1) * min(min(noise), min(col));
-for i = 1:1:length(noise)
-    img(floor((lon(i) - min(lon))/scale) + 1, floor((max(lat) - min(lat))/scale) - floor((lat(i) - min(lat))/scale) + 1) = col(i);
-end
-colormap('jet');
-imagesc(img);
-clear img;
-end
--- collarDisplay/README.md
+++
@@ -1,5 +0,0 @@
-Collar Display Tool
-===================
-
-The purpose of this tool is to display the processed collar signal data on a
-map.
