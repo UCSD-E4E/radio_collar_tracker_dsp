7ee64b57432b292383ebc5a55c1d0b3928a0471e
ntlhui <ntlhui@gmail.com>
2015-11-15 17:26:50 -0800

    Fixed indents

--- gps_logger/gps_keep_alive.py
+++ gps_logger/gps_keep_alive.py
@@ -2,6 +2,8 @@
 
 from pymavlink import mavutil
 import signal
+import os
+import argparse
 
 def handler(signum, frame):
 	global runstate
@@ -13,6 +15,15 @@ signal.signal(signal.SIGINT, handler)
 global runstate
 print("GPS_KEEPALIVE: Started")
 runstate = True
+
+gpio_export = open("/sys/class/gpio/export", 'w')
+# gpio_export.write('17\n')
+gpio_export.close()
+
+if not os.path.exists("/sys/class/gpio/gpio17"):
+	print("GPS_KEEPALIVE: Could not access LED!")
+
+
 
 parser = argparse.ArgumentParser(description = "MAVLink Keep Alive")
 parser.add_argument('-i', '--port', help = 'MAVLink port', metavar = 'port', dest = 'port', required = True)
@@ -27,18 +38,18 @@ while True:
 	if mavmaster.wait_heartbeat(blocking=False) is not None:
 		break
 	fail_counter += 1
-    if fail_counter > 1000:
-        print("GPS_KEEPALIVE: ERROR: Timeout connecting!")
-        sys.exit(1)
-    sleep(0.005)
+	if fail_counter > 1000:
+		print("GPS_KEEPALIVE: ERROR: Timeout connecting!")
+		sys.exit(1)
+	sleep(0.005)
 
 print("GPS_KEEPALIVE: Connected")
 
 mavmaster.mav.request_data_stream_send(mavmaster.target_system,
-        mavmaster.target_component, mavutil.mavlink.MAV_DATA_STREAM_POSITION,
-        10, 1)
+		mavmaster.target_component, mavutil.mavlink.MAV_DATA_STREAM_POSITION,
+		10, 1)
 
 print("GPS_KEEPALIVE: Running")
 while runstate:
-    msg = mavmaster.recv_match(blocking=True, timeout = 10)
+	msg = mavmaster.recv_match(blocking=True, timeout = 10)
 print("GPS_KEEPALIVE: Ending thread")
