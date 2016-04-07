b3dc8b11efcb19274575775b33ccd773f8b8d692
ntlhui <ntlhui@gmail.com>
2016-04-07 16:56:42 -0700

    Added GPL notice

--- src/sdr_record/raw_data_to_complex.c
+++ src/sdr_record/raw_data_to_complex.c
@@ -1,3 +1,20 @@
+/*
+RCT Payload Software
+Copyright (C) 2016  Hui, Nathan T.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -48,7 +65,7 @@ int main(int argc, char** argv){
 	fclose(infile);
 	fclose(outfile);
 	printf("Read %lu bytes, wrote %lu bytes\n", totread * sizeof(char), totread * sizeof(float));
-	
+
 	// get file size
 	return 0;
 }
