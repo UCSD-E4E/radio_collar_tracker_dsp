6017015e5d6fa595f8ece98848f65fbf6f069019
ntlhui <ntlhui@gmail.com>
2016-04-30 15:52:17 -0700

    Added build script for the doc

---
+++ doc/mkdoc.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+pdflatex -halt-on-error rct_manual.tex -output-directory .
+makeglossaries rct_manual
+pdflatex -halt-on-error rct_manual.tex -output-directory .
+pdflatex -halt-on-error rct_manual.tex -output-directory .
+rm *.aux *.glg *.glo *.gls *.ist *.log *.out *.toc
