ec383b7399eac80f0808a38673071089abee27f6
ntlhui <ntlhui@gmail.com>
2016-05-02 14:58:33 -0700

    Updated for interlinked glossary

--- doc/mkdoc.sh
+++ doc/mkdoc.sh
@@ -2,5 +2,6 @@
 pdflatex -halt-on-error rct_manual.tex -output-directory .
 makeglossaries rct_manual
 pdflatex -halt-on-error rct_manual.tex -output-directory .
-# pdflatex -halt-on-error rct_manual.tex -output-directory .
+makeglossaries rct_manual
+pdflatex -halt-on-error rct_manual.tex -output-directory .
 rm *.aux *.glg *.glo *.gls *.ist *.log *.toc
