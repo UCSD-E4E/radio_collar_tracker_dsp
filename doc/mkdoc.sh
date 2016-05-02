ef5b9aac40069c459c546d36b36bf02b16b198f3
ntlhui <ntlhui@gmail.com>
2016-05-02 14:32:26 -0700

    Removed extra latex call, removed .out files

--- doc/mkdoc.sh
+++ doc/mkdoc.sh
@@ -2,5 +2,5 @@
 pdflatex -halt-on-error rct_manual.tex -output-directory .
 makeglossaries rct_manual
 pdflatex -halt-on-error rct_manual.tex -output-directory .
-pdflatex -halt-on-error rct_manual.tex -output-directory .
-rm *.aux *.glg *.glo *.gls *.ist *.log *.out *.toc
+# pdflatex -halt-on-error rct_manual.tex -output-directory .
+rm *.aux *.glg *.glo *.gls *.ist *.log *.toc
