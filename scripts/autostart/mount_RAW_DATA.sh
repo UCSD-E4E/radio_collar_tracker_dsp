ce2b0c1d1b55d814e144c8b47a33dc0bf53d6611
ntlhui <ntlhui@gmail.com>
2016-07-27 17:05:48 -0700

    Set up service handles

--- scripts/autostart/mount_RAW_DATA.sh
+++ scripts/autostart/mount_RAW_DATA.sh
@@ -7,5 +7,22 @@
 # Default-Stop: 0 1 6
 # Short-Description: Mount RAW_DATA partition to /media/RAW_DATA
 ### END INIT INFO
-mount /dev/sda1 /media/RAW_DATA -o dmask=000,fmask=111
-exit
+
+PATH=/sbin:/bin:/usr/sbin:/usr/bin
+DESC="RCT RAW_DATA Daemon"
+
+case "$1" in
+	start)
+		mount /dev/sda1 /media/RAW_DATA -o dmask=000,fmask=111
+		;;
+	stop)
+		umount /media/RAW_DATA
+		;;
+	restart)
+		umount /media/RAW_DATA
+		mount /dev/sda1 /media/RAW_DATA -o dmask=000,fmask=111
+		;;
+	start)
+		;;
+esac
+exit 0
