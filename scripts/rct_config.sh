fbb54701b334c2b7cc07b9d6a7d7164002d77bd0
Nathan Hui <ntlhui@gmail.com>
2017-05-04 10:25:10 -0700

    Reconfigured to CI testing parameters for defaults

--- scripts/rct_config.sh
+++ scripts/rct_config.sh
@@ -1,7 +1,7 @@
 freq=172464000
-sampling_freq=2048000
-gain="19.7"
+sampling_freq=2000000
+gain="10.0"
 gps_port="/dev/ttyACM0"
 gps_baud=9600
-output_dir="/media/RAW_DATA/rct"
-log_dir="/home/pi"
+output_dir="/media/e4e/8076-430E"
+log_dir="/home/e4e"
