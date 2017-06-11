6a3d263c98ba797d65454aacf5fe5ffefa9c8475
Nathan Hui <ntlhui@gmail.com>
2017-06-10 21:27:33 -0700

    Removed old build script

--- doc/mkdoc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-pdflatex -halt-on-error rct_manual.tex -output-directory .
-makeglossaries rct_manual
-pdflatex -halt-on-error rct_manual.tex -output-directory .
-makeglossaries rct_manual
-pdflatex -halt-on-error rct_manual.tex -output-directory .
-rm *.aux *.glg *.glo *.gls *.ist *.log *.toc
