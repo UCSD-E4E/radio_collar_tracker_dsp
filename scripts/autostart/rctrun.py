2499a4ad76f71f2a29beb1877a12d07cacbe44b6
Nathan Hui <ntlhui@gmail.com>
2017-07-26 19:17:15 -0700

    Added check output_dir state machine

--- scripts/autostart/rctrun.py
+++ scripts/autostart/rctrun.py
@@ -35,6 +35,38 @@ def init_SDR():
 				return 1
 	return 1
 
+class OUTPUT_DIR_STATES(Enum):
+	get_output_dir = 0
+	check_output_dir = 1
+	check_space = 2
+	wait_recycle = 3
+
+def init_output_dir():
+	global thread_op
+	init_output_dir_state = OUTPUT_DIR_STATES.get_output_dir
+	while thread_op:
+		if init_output_dir_state == OUTPUT_DIR_STATES.get_output_dir:
+			output_dir = os.environ['output_dir']
+			init_output_dir_state = OUTPUT_DIR_STATES.check_output_dir
+		elif init_output_dir_state == OUTPUT_DIR_STATES.check_output_dir:
+			if os.path.isdir(output_dir):
+				init_output_dir_state = OUTPUT_DIR_STATES.check_space
+			else:
+				init_output_dir_state = OUTPUT_DIR_STATES.wait_recycle
+		elif init_output_dir_state == OUTPUT_DIR_STATES.check_space:
+			df = subprocess.Popen(['df', output_dir], stdout=subprocess.PIPE)
+			output = df.communicate()[0]
+			device, size, used, available, percent, mountpoint = output.split('\n')[1].split()
+			if available > 20 * 60 * 2000000 * 4:
+				# enough space
+				return 0
+			else:
+				init_output_dir_state = OUTPUT_DIR_STATES.wait_recycle
+		elif init_output_dir_state == OUTPUT_DIR_STATES.wait_recycle:
+			time.sleep(1)
+			init_output_dir_state == OUTPUT_DIR_STATES.check_output_dir
+	return 1
+	
 def sigint_handler(signal, frame):
 	print("REceived sig")
 	global thread_op
@@ -44,7 +76,9 @@ def main():
 	signal.signal(signal.SIGINT, sigint_handler)
 	signal.signal(signal.SIGTERM, sigint_handler)
 	init_SDR_thread = threading.Thread(target=init_SDR)
+	init_output_thread = threading.Thread(target=init_output_dir)
 	init_SDR_thread.start()
+	init_output_thread.start()
 	# init_SDR_thread.join()
 	signal.pause()
 
