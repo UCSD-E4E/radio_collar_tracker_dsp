37bf856860dc041001da4d0f404c982585fe4591
Nathan Hui <ntlhui@gmail.com>
2017-08-01 10:00:39 -0700

    Added ready and fail states

--- scripts/autostart/rctrun.py
+++ scripts/autostart/rctrun.py
@@ -7,19 +7,35 @@ import os
 import signal
 import serial
 import pynmea2
+import mmap
+
+WAIT_COUNT = 60
+
+devnull = open(os.devnull, 'w')
+thread_op = True
+mmap_file = None
+shared_states = None
+
+def get_var(var):
+	var_file = open('/usr/local/etc/rct_config')
+	for line in var_file:
+		if line.split('=')[0] == var:
+			value = line.split('=')[1]
+			return value.strip().strip('"').strip("'")
+	return None
 
 class SDR_INIT_STATES(Enum):
 	find_devices = 0
 	wait_recycle = 1
 	usrp_probe = 2
-
-devnull = open(os.devnull, 'w')
-thread_op = True
+	rdy = 3
+	fail = 4
 
 def init_SDR():
 	global thread_op
 	init_sdr_state = SDR_INIT_STATES.find_devices
 	while thread_op:
+		shared_states[0] = str(unichr(init_sdr_state.value))
 		if init_sdr_state == SDR_INIT_STATES.find_devices:
 			uhd_find_dev_retval = subprocess.call(['/usr/local/bin/uhd_find_devices', '--args=\"type=b200\"'], stdout=devnull, stderr=devnull)
 			if uhd_find_dev_retval == 0:
@@ -32,23 +48,27 @@ def init_SDR():
 		elif init_sdr_state == SDR_INIT_STATES.usrp_probe:
 			uhd_usrp_probe_retval = subprocess.call(['/usr/local/bin/uhd_usrp_probe', '--args=\"type=b200\"', '--init-only'], stdout=devnull, stderr=devnull)
 			if uhd_usrp_probe_retval == 0:
-				return 0
+				init_sdr_state = SDR_INIT_STATES.rdy
 			else:
-				return 1
-	return 1
+				init_sdr_state = SDR_INIT_STATES.fail
+	return 0
 
 class OUTPUT_DIR_STATES(Enum):
 	get_output_dir = 0
 	check_output_dir = 1
 	check_space = 2
 	wait_recycle = 3
+	rdy = 4
+	fail = 5
 
 def init_output_dir():
 	global thread_op
 	init_output_dir_state = OUTPUT_DIR_STATES.get_output_dir
+	counter = 0
 	while thread_op:
+		shared_states[1] = str(unichr(init_output_dir_state.value))
 		if init_output_dir_state == OUTPUT_DIR_STATES.get_output_dir:
-			output_dir = os.environ['output_dir']
+			output_dir = get_var('output_dir')
 			init_output_dir_state = OUTPUT_DIR_STATES.check_output_dir
 		elif init_output_dir_state == OUTPUT_DIR_STATES.check_output_dir:
 			if os.path.isdir(output_dir):
@@ -61,18 +81,24 @@ def init_output_dir():
 			device, size, used, available, percent, mountpoint = output.split('\n')[1].split()
 			if available > 20 * 60 * 2000000 * 4:
 				# enough space
-				return 0
+				init_output_dir_state = OUTPUT_DIR_STATES.rdy
 			else:
 				init_output_dir_state = OUTPUT_DIR_STATES.wait_recycle
 		elif init_output_dir_state == OUTPUT_DIR_STATES.wait_recycle:
 			time.sleep(1)
-			init_output_dir_state == OUTPUT_DIR_STATES.check_output_dir
-	return 1
+			counter = counter + 1
+			if counter > WAIT_COUNT:
+				init_output_dir_state = OUTPUT_DIR_STATES.fail
+			else:
+				init_output_dir_state = OUTPUT_DIR_STATES.check_output_dir
+	return 0
 
 class GPS_STATES(Enum):
 	get_tty = 0
 	get_msg = 1
 	wait_recycle = 2
+	rdy = 3
+	fail = 4
 
 def accept_gps(msg):
 	if msg.gps_qual == 0:
@@ -88,10 +114,12 @@ def accept_gps(msg):
 def init_gps():
 	global thread_op
 	init_gps_state = GPS_STATES.get_tty
+	counter = 0
 	while thread_op:
+		shared_states[2] = str(unichr(init_gps_state.value))
 		if init_gps_state == GPS_STATES.get_tty:
-			tty_device = os.environ['gps_port']
-			tty_baud = os.environ['gps_baud']
+			tty_device = get_var('gps_port')
+			tty_baud = get_var('gps_baud')
 			try:
 				tty_stream = serial.Serial(tty_device, tty_baud, timeout = 5)
 			except serial.SerialException, e:
@@ -113,7 +141,7 @@ def init_gps():
 				if msg.sentence_type == 'GGA':
 					if accept_gps(msg):
 						# good GPS
-						return 0
+						init_gps_state = GPS_STATES.rdy
 					else:
 						init_gps_state = GPS_STATES.wait_recycle
 				else:
@@ -122,22 +150,40 @@ def init_gps():
 				init_gps_state = GPS_STATES.get_msg
 		elif init_gps_state == GPS_STATES.wait_recycle:
 			time.sleep(1)
-			init_gps_state = GPS_STATES.get_msg
+			if counter > WAIT_COUNT:
+				init_gps_state = GPS_STATES.fail
+			else:
+				init_gps_state = GPS_STATES.get_msg
+	return 0
 
 def sigint_handler(signal, frame):
-	print("REceived sig")
+	print("Received sig")
 	global thread_op
 	thread_op = False
 
 def main():
+	# Set up mmap files
+	if not os.path.isdir('/var/local/rct'):
+		os.makedirs('/var/local/rct')
+	mmap_file = open('/var/local/rct/status.dat', 'w+b')
+	mmap_file.write('\0\0\0')
+	mmap_file.flush()
+	global shared_states
+	shared_states = mmap.mmap(mmap_file.fileno(), 3)
 	signal.signal(signal.SIGINT, sigint_handler)
 	signal.signal(signal.SIGTERM, sigint_handler)
 	init_SDR_thread = threading.Thread(target=init_SDR)
 	init_output_thread = threading.Thread(target=init_output_dir)
+	init_gps_thread = threading.Thread(target=init_gps)
 	init_SDR_thread.start()
 	init_output_thread.start()
-	# init_SDR_thread.join()
+	init_gps_thread.start()
 	signal.pause()
+	init_SDR_thread.join()
+	init_output_thread.join()
+	init_gps_thread.join()
+	shared_states.close()
+	mmap_file.close()
 
 if __name__ == '__main__':
 	main()
