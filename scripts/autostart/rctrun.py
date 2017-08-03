698e2eafb42f676eb78b560eab1774de460b93f6
Nathan Hui <ntlhui@gmail.com>
2017-08-02 19:23:39 -0700

    Added switch state machine

--- scripts/autostart/rctrun.py
+++ scripts/autostart/rctrun.py
@@ -8,14 +8,48 @@ import signal
 import serial
 import pynmea2
 import mmap
-# import mraa
+import sys
+import mraa
 
 WAIT_COUNT = 60
+SW1_PIN = 51
 
-devnull = open(os.devnull, 'w')
-thread_op = True
+devnull = None
+init_thread_op = True
+master_thread_op = True
 mmap_file = None
 shared_states = None
+
+class SDR_INIT_STATES(Enum):
+	find_devices = 0
+	wait_recycle = 1
+	usrp_probe = 2
+	rdy = 3
+	fail = 4
+
+class GPS_STATES(Enum):
+	get_tty = 0
+	get_msg = 1
+	wait_recycle = 2
+	rdy = 3
+	fail = 4
+
+class OUTPUT_DIR_STATES(Enum):
+	get_output_dir = 0
+	check_output_dir = 1
+	check_space = 2
+	wait_recycle = 3
+	rdy = 4
+	fail = 5
+
+class RCT_STATES(Enum):
+	init		=	0
+	wait_init	=	1
+	wait_start	=	2
+	start		=	3
+	wait_end	=	4
+	finish		=	5
+	fail		=	6
 
 def get_var(var):
 	var_file = open('/usr/local/etc/rct_config')
@@ -25,17 +59,11 @@ def get_var(var):
 			return value.strip().strip('"').strip("'")
 	return None
 
-class SDR_INIT_STATES(Enum):
-	find_devices = 0
-	wait_recycle = 1
-	usrp_probe = 2
-	rdy = 3
-	fail = 4
-
 def init_SDR():
-	global thread_op
+	global init_thread_op
+	global shared_states
 	init_sdr_state = SDR_INIT_STATES.find_devices
-	while thread_op:
+	while init_thread_op:
 		shared_states[0] = str(unichr(init_sdr_state.value))
 		if init_sdr_state == SDR_INIT_STATES.find_devices:
 			uhd_find_dev_retval = subprocess.call(['/usr/local/bin/uhd_find_devices', '--args=\"type=b200\"'], stdout=devnull, stderr=devnull)
@@ -54,19 +82,12 @@ def init_SDR():
 				init_sdr_state = SDR_INIT_STATES.fail
 	return 0
 
-class OUTPUT_DIR_STATES(Enum):
-	get_output_dir = 0
-	check_output_dir = 1
-	check_space = 2
-	wait_recycle = 3
-	rdy = 4
-	fail = 5
-
 def init_output_dir():
-	global thread_op
+	global init_thread_op
+	global shared_states
 	init_output_dir_state = OUTPUT_DIR_STATES.get_output_dir
 	counter = 0
-	while thread_op:
+	while init_thread_op:
 		shared_states[1] = str(unichr(init_output_dir_state.value))
 		if init_output_dir_state == OUTPUT_DIR_STATES.get_output_dir:
 			output_dir = get_var('output_dir')
@@ -94,14 +115,8 @@ def init_output_dir():
 				init_output_dir_state = OUTPUT_DIR_STATES.check_output_dir
 	return 0
 
-class GPS_STATES(Enum):
-	get_tty = 0
-	get_msg = 1
-	wait_recycle = 2
-	rdy = 3
-	fail = 4
-
 def accept_gps(msg):
+	return True
 	if msg.gps_qual == 0:
 		return False
 	if msg.gps_qual == 7:
@@ -113,11 +128,12 @@ def accept_gps(msg):
 	return True
 
 def init_gps():
-	global thread_op
+	global init_thread_op
+	global shared_states
 	init_gps_state = GPS_STATES.get_tty
 	counter = 0
 	msg_counter = 0
-	while thread_op:
+	while init_thread_op:
 		shared_states[2] = str(unichr(init_gps_state.value))
 		if init_gps_state == GPS_STATES.get_tty:
 			tty_device = get_var('gps_port')
@@ -164,34 +180,113 @@ def init_gps():
 				init_gps_state = GPS_STATES.get_msg
 	return 0
 
-def sigint_handler(signal, frame):
-	print("Received sig")
-	global thread_op
-	thread_op = False
-
-def main():
-	# Set up mmap files
-	if not os.path.isdir('/var/local/rct'):
-		os.makedirs('/var/local/rct')
-	mmap_file = open('/var/local/rct/status.dat', 'w+b')
-	mmap_file.write('\0\0\0')
-	mmap_file.flush()
+def init_state_complete():
 	global shared_states
-	shared_states = mmap.mmap(mmap_file.fileno(), 3)
-	signal.signal(signal.SIGINT, sigint_handler)
-	signal.signal(signal.SIGTERM, sigint_handler)
-	init_SDR_thread = threading.Thread(target=init_SDR)
-	init_output_thread = threading.Thread(target=init_output_dir)
-	init_gps_thread = threading.Thread(target=init_gps)
-	init_SDR_thread.start()
-	init_output_thread.start()
-	init_gps_thread.start()
-	signal.pause()
+	return SDR_INIT_STATES(ord(shared_states[0])) == SDR_INIT_STATES.rdy and OUTPUT_DIR_STATES(ord(shared_states[1])) == OUTPUT_DIR_STATES.rdy and GPS_STATES(ord(shared_states[2])) == GPS_STATES.rdy
+
+def init_RCT():
+	global master_thread_op
+	global init_thread_op
+	init_RCT_state = RCT_STATES.init
+	if 'mraa' in sys.modules:
+		switch_handle = mraa.Gpio(SW1_PIN)
+		switch_handle.dir(mraa.DIR_IN)
+	else:
+		switch_file = open('switch', 'a+')
+		switch_file.write('0')
+		switch_file.flush()
+		switch_handle = mmap.mmap(switch_file.fileno(), 1)
+	while master_thread_op:
+		shared_states[3] = str(unichr(init_RCT_state.value))
+		if init_RCT_state == RCT_STATES.init:
+			init_SDR_thread = threading.Thread(target=init_SDR)
+			init_output_thread = threading.Thread(target=init_output_dir)
+			init_gps_thread = threading.Thread(target=init_gps)
+			init_SDR_thread.start()
+			init_output_thread.start()
+			init_gps_thread.start()
+			init_RCT_state = RCT_STATES.wait_init
+		elif init_RCT_state == RCT_STATES.wait_init:
+			# wait for init state machines to complete
+			init_state = init_state_complete()
+			if init_state:
+				init_RCT_state = RCT_STATES.wait_start
+			else:
+				init_RCT_state = RCT_STATES.wait_init
+		elif init_RCT_state == RCT_STATES.wait_start:
+			if 'mraa' in sys.modules:
+				switch_state = switch_handle.read()
+			else:
+				switch_state = switch_handle[0]
+			if switch_state:
+				init_RCT_state = RCT_STATES.start
+				init_thread_op = False
+				init_SDR_thread.join()
+				init_output_thread.join()
+				init_gps_thread.join()
+			else:
+				init_RCT_state = RCT_STATES.wait_start
+		elif init_RCT_state == RCT_STATES.start:
+			# sdr_starter = subprocess.Popen(['sdr_starter'])
+			sdr_starter = subprocess.Popen(['&INSTALL_PREFIX/sdr_starter'])
+			init_RCT_state = RCT_STATES.wait_end
+		elif init_RCT_state == RCT_STATES.wait_end:
+			if 'mraa' in sys.modules:
+				switch_state = switch_handle.read()
+			else:
+				switch_state = switch_handle[0]
+			if switch_state:
+				init_RCT_state = RCT_STATES.wait_end
+			else:
+				init_RCT_state = RCT_STATES.finish
+		elif init_RCT_state == RCT_STATES.finish:
+			sdr_starter.terminate()
+			rct_retval = sdr_starter.wait()
+			if not rct_retval:
+				init_RCT_state = RCT_STATES.fail
+			subprocess.call(['sync'])
+			init_RCT_state = RCT_STATES.init
+		pass
+	if 'mraa' not in sys.modules:
+		switch_file.close()
+		switch_handle.close()
 	init_SDR_thread.join()
 	init_output_thread.join()
 	init_gps_thread.join()
+
+def sigint_handler(signal, frame):
+	print("Received sig")
+	global init_thread_op
+	global master_thread_op
+	init_thread_op = False
+	master_thread_op = False
+
+def main():
+	# Set up mmap files
+	global shared_states
+	if not os.path.isdir('/var/local/rct'):
+		os.makedirs('/var/local/rct')
+	mmap_file = open('/var/local/rct/status.dat', 'w+b')
+	mmap_file.write('\0' * 4)
+	mmap_file.flush()
+	shared_states = mmap.mmap(mmap_file.fileno(), 4)
+
+	devnull = open(os.devnull, 'w')
+
+	signal.signal(signal.SIGINT, sigint_handler)
+	signal.signal(signal.SIGTERM, sigint_handler)
+
+	init_RCT_thread = threading.Thread(target=init_RCT)
+	init_RCT_thread.start()
+
+	signal.pause()
+
+	init_RCT_thread.join()
+	
+
 	shared_states.close()
 	mmap_file.close()
+	devnull.close()
 
 if __name__ == '__main__':
 	main()
