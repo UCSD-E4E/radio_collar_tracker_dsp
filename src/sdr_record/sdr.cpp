2b64c0daaa8ca1e1f7ded276eb503faf8413a93c
Nathan Hui <ntlhui@gmail.com>
2018-12-25 15:44:06 -0800

    Added C++ linkage

--- src/sdr_record/sdr.cpp
+++ src/sdr_record/sdr.cpp
@@ -1,12 +1,16 @@
 #include "sdr.hpp"
+#include "iq_data.hpp"
+#include <syslog.h>
+#include <signal.h>
+#include <uhd.h>
+#include <sys/time.h>
+#include <error.h>
 #include <iostream>
 #include <stdexcept>
 #include <complex>
 #include <thread>
-#include <syslog.h>
-#include <signal.h>
-#include <uhd.h>
-#include <error.h>
+#include <memory>
+#include <condition_variable>
 
 namespace RTT{
 	SDR::SDR(double gain, long int rate, long int freq) : device_args(""), 
@@ -17,7 +21,10 @@ namespace RTT{
 		//create a usrp device
 		syslog(LOG_DEBUG, "Creating USRP with args \"%s\"...\n", 
 			device_args.c_str());
-		uhd_usrp_make(&usrp, device_args.c_str());
+		uhd_error error = uhd_usrp_make(&usrp, device_args.c_str());
+		if(error != UHD_ERROR_NONE){
+			syslog(LOG_ERR, "UHD Error: %s", uhd_strerror(error).c_str());
+		}
 		uhd_rx_streamer_make(&rx_streamer);
 
 		//set the sample rate
@@ -32,8 +39,8 @@ namespace RTT{
 		uhd_usrp_get_rx_rate(usrp, channel, &value);
 		syslog(LOG_DEBUG, "Actual RX Rate: %f Msps...\n", (value / 1e6));
 
-		if(value / rate > 0.01){
-			syslog(LOG_WARNING, "WARNING: RX rate not correctly set!");
+		if((value / rate) - 1.0 > 0.01){
+			syslog(LOG_WARNING, "WARNING: RX rate not correctly set, actual is %f!", value);
 		}
 
 		// set the rf gain
@@ -56,17 +63,20 @@ namespace RTT{
 
 	}
 
-	void SDR::startStreaming(std::queue<std::vector<std::complex<short>>*>& queue, std::mutex& mutex, const volatile bool* die){
+	void SDR::startStreaming(std::queue<IQdataPtr>& queue, std::mutex& mutex, 
+		std::condition_variable& var, const volatile bool* ndie){
 		output_queue = &queue;
 		output_mutex = &mutex;
-		stream_thread = new std::thread(&SDR::streamer, this, die);
+		output_var = &var;
+		stream_thread = new std::thread(&SDR::streamer, this, ndie);
 	}
 
 	void SDR::stopStreaming(){
 		stream_thread->join();
+		delete stream_thread;
 	}
 
-	std::string uhd_strerror(uhd_error err){
+	std::string SDR::uhd_strerror(uhd_error err){
 		switch(err){
 			case UHD_ERROR_NONE:
 				return "None";
@@ -132,6 +142,7 @@ namespace RTT{
 			char* errbuf = new char[1024];
 			retval = uhd_get_last_error(errbuf, 1024);
 			syslog(LOG_ERR, "UHD reports %s", errbuf);
+			delete[] errbuf;
 			return;
 		}
 		
@@ -141,25 +152,36 @@ namespace RTT{
 		
 		syslog(LOG_DEBUG, "sdr streamer issuing stream command");
 		uhd_rx_streamer_issue_stream_cmd(rx_streamer, &stream_cmd);
+		struct timeval reftime;
+		gettimeofday(&reftime, NULL);
+		uhd_usrp_set_time_now(usrp, reftime.tv_sec, (double) reftime.tv_usec / 1e6, 0);
 
 		uhd_rx_metadata_handle md{};
 		uhd_rx_metadata_make(&md);
-		std::vector<std::complex<short>>* buff;
 		size_t total_samples = 0;
 
 		assert(ndie);
+
+		struct timeval starttime;
+		struct timeval stoptime;
+		struct timeval finishtime;
 
 		size_t num_samps = 0;
 		uhd_rx_metadata_error_code_t error_code;
 		int16_t* raw_buffer = new int16_t[rx_buffer_size * 2];
 		syslog(LOG_DEBUG, "Starting main loop");
 		while(*ndie){
-			buff = new std::vector<std::complex<short>>(rx_buffer_size);
+
+			IQdataPtr databuf (new IQdata(rx_buffer_size));
+			gettimeofday(&starttime, NULL);
 			uhd_rx_streamer_recv(rx_streamer, (void**) &raw_buffer, rx_buffer_size, &md, 1.0, false, &num_samps);
+			gettimeofday(&stoptime, NULL);
 
 			for(size_t i = 0; i < rx_buffer_size; i++){
-				(*buff)[i] = std::complex<short>(raw_buffer[2 * i], raw_buffer[2 * i + 1]);
+				(*(databuf->data))[i] = std::complex<short>(raw_buffer[2 * i], raw_buffer[2 * i + 1]);
 			}
+			databuf->time_ms = (uint64_t)(md->rx_metadata_cpp.time_spec.get_real_secs() * 1e3);
+
 
 			uhd_rx_metadata_error_code(md, &error_code);
 
@@ -175,18 +197,29 @@ namespace RTT{
 				char* strbuf = new char[1024];
 				uhd_rx_metadata_strerror(md, strbuf, 1024);
 				syslog(LOG_ERR, "Receiver error: %s", strbuf);
+				delete[] strbuf;
 			}
 			total_samples += num_samps;
 
-			std::lock_guard<std::mutex> guard(*output_mutex);
-			output_queue->push(buff);
+			std::unique_lock<std::mutex> guard(*output_mutex);
+			output_queue->push(databuf);
+			guard.unlock();
+			output_var->notify_all();
+			gettimeofday(&finishtime, NULL);
+
+			uint64_t starttimeus = starttime.tv_sec * 1e6 + starttime.tv_usec;
+			uint64_t stoptimeus = stoptime.tv_sec * 1e6 + stoptime.tv_usec;
+			uint64_t finishtimeus = finishtime.tv_sec * 1e6 + finishtime.tv_usec;
+
+			syslog(LOG_DEBUG, "%lu us for record, %lu us for queue\n", stoptimeus - starttimeus, finishtimeus - stoptimeus);
+
 		}
 		syslog(LOG_DEBUG, "Stopping loop");
-		buff = new std::vector<std::complex<short>>(rx_buffer_size);
+		IQdataPtr databuf(new IQdata(rx_buffer_size));
 		uhd_rx_streamer_recv(rx_streamer, (void**) &raw_buffer, rx_buffer_size, &md, 1.0, false, &num_samps);
 
 		for(size_t i = 0; i < rx_buffer_size; i++){
-			(*buff)[i] = std::complex<short>(raw_buffer[2 * i], raw_buffer[2 * i + 1]);
+			(*(databuf->data))[i] = std::complex<short>(raw_buffer[2 * i], raw_buffer[2 * i + 1]);
 		}
 
 		uhd_rx_metadata_error_code(md, &error_code);
@@ -194,19 +227,43 @@ namespace RTT{
 			char* strbuf = new char[1024];
 			uhd_rx_metadata_strerror(md, strbuf, 1024);
 			syslog(LOG_ERR, "Receiver error: %s", strbuf);
+			delete[] strbuf;
 		}
 		total_samples += num_samps;
 
 		std::lock_guard<std::mutex>* guard = new std::lock_guard<std::mutex>(*output_mutex);
-		output_queue->push(buff);
-		delete(guard);
+		output_queue->push(databuf);
+		delete guard;
+		output_var->notify_all();
 
 		stream_cmd.stream_mode =  UHD_STREAM_MODE_STOP_CONTINUOUS;
 		uhd_rx_streamer_issue_stream_cmd(rx_streamer, &stream_cmd);
 
 		syslog(LOG_INFO, "Received %zd samples, %.3f seconds of data", total_samples, (double)total_samples / rx_rate);
+		uhd_rx_metadata_free(&md);
+
+		delete[] raw_buffer;
+		delete[] channel_nums;
+		delete[] stream_args.args;
+		delete[] stream_args.otw_format;
+		delete[] stream_args.cpu_format;
 	}
 
 	SDR::~SDR(){
+		uhd_usrp_free(&usrp);
+		uhd_rx_streamer_free(&rx_streamer);
+	}
+
+	SDR::SDR(): device_args(""), 
+			subdev("A:A"), ant("RX2"), ref("internal"), cpu_format("sc16"), 
+			wire_format("sc16"), channel{0}, if_gain(0), rx_rate(2000000), 
+			rx_freq(0){
+		uhd_error error = uhd_usrp_make(&usrp, device_args.c_str());
+		if(error != UHD_ERROR_NONE){
+			syslog(LOG_ERR, "UHD Error: %s", uhd_strerror(error).c_str());
+		}
+		uhd_rx_streamer_make(&rx_streamer);
+
+
 	}
 }
--- src/sdr_record/sdr.hpp
+++ src/sdr_record/sdr.hpp
@@ -1,16 +1,17 @@
 #ifndef __SDR_H__
 #define __SDR_H__
 
+#include "iq_data.hpp"
+#include <uhd.h>
 #include <string>
 #include <queue>
 #include <mutex>
 #include <thread>
-#include <complex>
-#include <uhd.h>
+#include <condition_variable>
 
 namespace RTT{
-	typedef std::complex<short> short_cpx;
 	class SDR{
+
 		uhd_usrp_handle usrp;
 
 		size_t rx_buffer_size = 16384;
@@ -25,17 +26,22 @@ namespace RTT{
 		long int rx_rate;
 		long int rx_freq;
 		void streamer(const volatile bool* die);
-		std::queue<std::vector<std::complex<short>>*>* output_queue;
+		std::queue<IQdataPtr>* output_queue;
 		std::mutex* output_mutex;
+		std::condition_variable* output_var;
 		std::thread* stream_thread;
 
 		uhd_rx_streamer_handle rx_streamer;
+		std::string uhd_strerror(uhd_error err);
+	protected:
+		SDR();
 	public:
 		SDR(double gain, long int rate, long int freq);
 		~SDR();
 		void setBufferSize(size_t buff_size);
 		int getBufferSize();
-		void startStreaming(std::queue<std::vector<short_cpx>*>&, std::mutex&, const volatile bool* ndie);
+		void startStreaming(std::queue<IQdataPtr>&, std::mutex&, 
+			std::condition_variable&, const volatile bool* ndie);
 		void stopStreaming();
 	};
 }
