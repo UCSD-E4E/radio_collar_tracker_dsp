081b929ec4ea844cdb6a1b8dff6ab9d62bb1b90d
Eric Tran <e3tran@ucsd.edu>
2019-04-06 15:27:37 -0700

    Added LED Status Blinking

--- src/arduino/ui_core.cpp
+++ src/arduino/ui_core.cpp
@@ -73,24 +73,16 @@ void setup() {
 	yellow.ledstate = OFF;
 	green.ledstate = OFF;
 
-	// Set up timers
-	cli();//stop interrupts
-
-	//set timer1 interrupt at 1Hz
-	TCCR1A = 0;// set entire TCCR1A register to 0
-	TCCR1B = 0;// same for TCCR1B
-	TCNT1  = 0;//initialize counter value to 0
-	// set compare match register for 1hz increments
-	//OCR1A = 15624;// = (16*10^6) / (1*1024) - 1 (must be <65536)
-	OCR1A = 3124;// 15624 / 5 (must be 0.2 seconds)
-	// turn on CTC mode
+	// Set up timer
+	cli();
+	TCCR1A = 0;
+	TCCR1B = 0;
+	TCNT1  = 0;
+	OCR1A = 3124;
 	TCCR1B |= (1 << WGM12);
-	// Set CS12 and CS10 bits for 1024 prescaler
 	TCCR1B |= (1 << CS12) | (1 << CS10);  
-	// enable timer compare interrupt
 	TIMSK1 |= (1 << OCIE1A);
-
-	sei();//allow interrupts
+	sei();
 
         // Broken!!!
         // compass.begin(NULL);
@@ -99,18 +91,8 @@ void setup() {
         // sensor = new Sensor_Module(&status.gps);
 }
 
-
-
-
 inline void configureLED(){
 }
-
-
-
-
-
-
-
 
 ISR( TIMER1_COMPA_vect ) { //timer1 interrupt 1Hz
 	count++;
@@ -211,17 +193,6 @@ ISR( TIMER1_COMPA_vect ) { //timer1 interrupt 1Hz
 		count = 0;
 	}
 }
-
-			
-
-
-
-
-
-
-
-
-
 
 void loop() {
         if (Serial1.available() > 0){
