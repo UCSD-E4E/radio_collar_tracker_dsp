f1046c4bd32c625e29f67a797cc3500d3d03b35e
Nathan Hui <ntlhui@gmail.com>
2019-07-15 19:45:30 -0700

    Removed old modules, wrote new pipeline with FFT first

--- src/sdr_record/dspv3.cpp
+++ src/sdr_record/dspv3.cpp
@@ -4,7 +4,16 @@
 #include <iostream>
 #include <cstdio>
 #include <fftw3.h>
-// #define DEBUG
+#include <cassert>
+#include <float.h>
+#include <boost/circular_buffer.hpp>
+#include <boost/accumulators/accumulators.hpp>
+#include <boost/accumulators/statistics.hpp>
+#include <stdexcept>
+#include "AbstractSDR.hpp"
+
+
+#define DEBUG
 
 #ifdef DEBUG
 #include <iostream>
@@ -12,18 +21,75 @@
 
 namespace RTT{
 	DSP_V3::DSP_V3(const std::size_t sampling_freq, 
-		const std::size_t center_freq) : _iq_data_queue(), _iq_mux(), _iq_cv(),
-		_fir(), _mag_data_queue(), _mag_mux(), _mag_cv(),
-		int_factor(int_time_s * sampling_freq), 
-		_int(int_factor), _candidate_queue(), _can_mux(), _can_cv(),
-		_clfr(0, (double) sampling_freq / int_factor, sampling_freq, 
-			center_freq), _output_fmt{nullptr}{
-		
+		const std::size_t center_freq,
+		const std::vector<std::size_t>& target_freqs) : 
+	target_freqs(target_freqs),
+	nFreqs(target_freqs.size()),
+	s_freq{sampling_freq},
+	c_freq{center_freq}{
+
+		int_factor = int_time_s * sampling_freq / FFT_LEN;
+		clfr_input_freq = (double) sampling_freq / int_factor / FFT_LEN;
+		maximizer_len = 0.1 * clfr_input_freq;
+		median_len = 0.3 * clfr_input_freq;
+		ping_width_samp = ping_width_ms * sampling_freq / 1000. / FFT_LEN / int_factor;
+		data_len = ping_width_samp * 2;
+		_ms_per_sample = 1/((double) sampling_freq / int_factor) * 1000.0;
+
+
+		for(auto it = target_freqs.begin(); it != target_freqs.end(); it++){
+			if((std::size_t)std::abs((int64_t)*it - (int64_t)center_freq) > sampling_freq / 2){
+				std::cerr << "Target frequency " << *it << " not reachable!" << std::endl;
+				throw std::invalid_argument{"Target frequency not reachable!"};
+			}
+			if(*it >= center_freq){
+				// upper half
+				target_bins.push_back((*it - center_freq) / (sampling_freq / 2) * FFT_LEN / 2);
+			}else{
+				// lower half
+				target_bins.push_back(FFT_LEN - (center_freq - *it) / (sampling_freq / 2) * FFT_LEN / 2);
+			}
+		}
+
+		_unpack_fft_in = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * FFT_LEN);
+		if(_unpack_fft_in == nullptr){
+			std::cout << "Failed to allocate _unpack_fft_in!" << std::endl;
+			return;
+		}
+
+
+		_unpack_fft_out = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * FFT_LEN);
+		if(_unpack_fft_out == nullptr){
+			std::cout << "Failed to allocation _unpack_fft_out!" << std::endl;
+			return;
+		}
+
+		if(!fftw_init_threads()){
+			std::cout << "Failed to initialize FFTW threads!" << std::endl;
+			return;
+		}
+		fftw_plan_with_nthreads(4);
+		_unpack_fft_plan = fftw_plan_dft_1d(FFT_LEN, _unpack_fft_in, _unpack_fft_out,
+			FFTW_FORWARD, FFTW_MEASURE);
 	}
 
 	DSP_V3::~DSP_V3(){
+		fftw_destroy_plan(_unpack_fft_plan);
+		fftw_free(_unpack_fft_out);
+		fftw_free(_unpack_fft_in);
+		fftw_forget_wisdom();
+		fftw_cleanup();
+		fftw_cleanup_threads();
 		if(_output_fmt != nullptr){
 			delete[] _output_fmt;
+		}
+	}
+
+	const std::size_t DSP_V3::idxToFreq(const std::size_t idx) const{
+		if(idx > FFT_LEN / 2){
+			return -idx / FFT_LEN * s_freq + c_freq;
+		}else{
+			return idx / FFT_LEN * s_freq + c_freq;
 		}
 	}
 
@@ -34,6 +100,9 @@ namespace RTT{
 		_run = true;
 		_thread = new std::thread(&DSP_V3::_unpack, this, std::ref(i_q), 
 			std::ref(i_m), std::ref(i_v));
+		_c_thread = new std::thread(&DSP_V3::classify, this, std::ref(_c_q),
+			std::ref(_c_m), std::ref(_c_v), std::ref(o_q), std::ref(o_m),
+			std::ref(o_v));
 		// _fir.start(_iq_data_queue, _iq_mux, _iq_cv, _mag_data_queue, _mag_mux,
 		// 	_mag_cv);
 		// _int.start(_mag_data_queue, _mag_mux, _mag_cv, _candidate_queue, 
@@ -42,40 +111,217 @@ namespace RTT{
 	}
 
 	void DSP_V3::stopProcessing(){
+		#ifdef DEBUG
+		std::cout << "DSP_V3 is stopping:" << std::endl;
+		#endif
 		_in_v->notify_all();
 		_run = false;
 		_thread->join();
 
-		#ifdef DEBUG
-		std::cout << "DSP_V3 is stopping:" << std::endl;
-		// std::cout << "_fir has " << _iq_data_queue.size() << " data waiting, stopping" << std::endl;
-		#endif
-		// _fir.stop();
-		#ifdef DEBUG
-		// std::cout << "_fir has " << _iq_data_queue.size() << " waiting after being stopped" << std::endl;
-		std::cout << "_int has " << _mag_data_queue.size() << " data waiting, stopping" << std::endl;
-		#endif
-		// _int.stop();
-		#ifdef DEBUG
-		std::cout << "_int has " << _mag_data_queue.size() << " waiting after being stopped" << std::endl;
-		// std::cout << "_clfr has " << _candidate_queue.size() << " data waiting, stopping" << std::endl;
-		#endif
-		// _clfr.stop();
-		// #ifdef DEBUG
-		// std::cout << "_clfr has " << _candidate_queue.size() << " waiting after being stopped" << std::endl;
-		// #endif
+		_c_v.notify_all();
+		_c_thread->join();
+
 
 		delete _thread;
 	}
 
-	std::size_t IQdataToDouble(std::complex<double> dataObj, std::vector<std::complex<double>>& data){
-		// std::vector<short_cpx>& short_vec = *dataObj->data;
-		// data.resize(short_vec.size());
-		std::size_t i = 0;
-		// for(; i < short_vec.size(); i++){
-			// data[i] = std::complex<double>(short_vec[i].real() / 4096.0, short_vec[i].imag() / 4096.0);
-		// }
-		return i;
+	std::shared_ptr<std::vector<double>> DSP_V3::max(boost::circular_buffer<std::shared_ptr<std::vector<double>>>& sig){
+		std::shared_ptr<std::vector<double>> retVal = std::make_shared<std::vector<double>>();
+		std::vector<double>& maxVal = *retVal;
+		maxVal.resize(sig[0]->size());
+		for(std::size_t i = 0; i < maxVal.size(); i++){
+			maxVal[i] = -DBL_MAX;
+		}
+		for(auto it = sig.begin(); it != sig.end(); it++){
+			for(std::size_t i = 0; i < maxVal.size(); i++){
+				maxVal[i] = std::max((*(*it))[i], maxVal[i]);
+			}
+		}
+		return retVal;
+	}
+
+	std::vector<double>* DSP_V3::sig_median(boost::circular_buffer<std::shared_ptr<std::vector<double>>>& sig){
+		
+		boost::accumulators::accumulator_set<double, boost::accumulators::features<boost::accumulators::tag::median> > acc[nFreqs];
+		for(auto it = sig.begin(); it != sig.end(); it++){
+			std::shared_ptr<std::vector<double>> ptr = *it;
+			std::vector<double>& vec= *ptr;
+			for(std::size_t i = 0; i < nFreqs; i++){
+				acc[i](vec[i]);
+			}
+		}
+		std::vector<double>* retval = new std::vector<double>{};
+		std::vector<double>& median = *retval;
+		median.resize(nFreqs);
+		for(std::size_t i = 0; i < nFreqs; i++){
+			median[i] = boost::accumulators::median(acc[i]);
+		}
+		return retval;
+	}
+
+	std::size_t _debug_ctr = 0;
+
+	std::shared_ptr<bool> DSP_V3::compare(
+		std::shared_ptr<std::vector<double>> data_ptr,
+		std::shared_ptr<std::vector<double>> threshold_ptr, double min_snr){
+
+		std::vector<double>& data = *data_ptr;
+		std::vector<double>& threshold = *threshold_ptr;
+		bool* retval = new bool[nFreqs];
+		for(std::size_t i = 0; i < nFreqs; i++){
+			retval[i] = (data[i] > threshold[i] + min_snr);
+		}
+		return std::shared_ptr<bool>(retval, std::default_delete<bool[]>());
+	}
+
+	std::shared_ptr<std::set<std::size_t>> DSP_V3::has_falling_edge(
+		boost::circular_buffer<std::shared_ptr<bool>>::iterator it){
+		std::shared_ptr<bool> s1 = *(it - 1);
+		std::shared_ptr<bool> s2 = *it;
+
+		std::shared_ptr<std::set<std::size_t>> retval_ptr = std::make_shared<std::set<std::size_t>>();
+		std::set<std::size_t>& retval = *retval_ptr;
+
+		for(std::size_t i = 0; i < nFreqs; i++){
+			if(s1.get()[i] == true && s2.get()[i] == false){
+				retval.insert(i);
+			}
+		}
+		return retval_ptr;
+	}
+
+	bool is_rising_edge(boost::circular_buffer<std::shared_ptr<bool>>::iterator it, std::size_t i){
+		if((*(it - 1)).get()[i] == false && (*it).get()[i] == true){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	const double DSP_V3::get_pulse_magnitude(
+			boost::circular_buffer<std::shared_ptr<std::vector<double>>>::iterator start,
+			boost::circular_buffer<std::shared_ptr<std::vector<double>>>::iterator end, 
+			std::size_t idx) const{
+		double max_amplitude = std::numeric_limits<double>::lowest();
+		for(;start != end; start++){
+			if((**start)[idx] > max_amplitude){
+				max_amplitude = (**start)[idx];
+			}
+		}
+		return max_amplitude;
+	}
+
+	const std::size_t DSP_V3::get_pulse_width(
+		boost::circular_buffer<std::shared_ptr<bool>>::iterator end,
+		boost::circular_buffer<std::shared_ptr<bool>>::iterator begin,
+		std::size_t i){
+
+		std::size_t width = 0;
+
+		while(!is_rising_edge(end, i)){
+			end--;
+			width++;
+			if(end == begin){
+				return -1;
+			}
+		}
+		return width;
+	}
+
+	void DSP_V3::classify(std::queue<std::shared_ptr<std::vector<double>>>& i_q, std::mutex& i_m,
+		std::condition_variable& i_v, std::queue<PingPtr>& o_q, std::mutex& o_m,
+		std::condition_variable& o_v){
+		
+		// Local vars
+		boost::circular_buffer<std::shared_ptr<std::vector<double>>> data{data_len};
+		boost::circular_buffer<std::shared_ptr<std::vector<double>>> pk_hist{maximizer_len};
+		boost::circular_buffer<std::shared_ptr<std::vector<double>>> peaks{median_len};
+		
+
+		std::size_t id_len = (std::size_t)(clfr_input_freq * 0.5);
+		boost::circular_buffer<std::shared_ptr<bool>> id_signal{id_len};
+		std::cout << id_len << " len id_len" << std::endl;
+		std::cout << maximizer_len << " len pk_hist" << std::endl;
+		std::cout << median_len << " len peaks" << std::endl;
+		for(std::size_t i = 0; i < id_len; i++){
+			std::shared_ptr<bool> id_sig(new bool[nFreqs], std::default_delete<bool[]>());
+			id_signal.push_back(id_sig);
+		}
+
+		std::size_t signal_idx = 0;
+		std::size_t out_count = 0;
+
+		std::shared_ptr<std::vector<double>> threshold_ptr(new std::vector<double>(nFreqs, 100));
+
+		while(_run || !i_q.empty()){
+			std::unique_lock<std::mutex> in_lock(i_m);
+			if(i_q.empty()){
+				i_v.wait(in_lock);
+			}
+
+			if(!i_q.empty()){
+				std::vector<double>& threshold = *threshold_ptr;
+				std::shared_ptr<std::vector<double>> sig = i_q.front();
+				i_q.pop();
+
+				if(threshold[0] != 100){
+					data.push_back(sig);
+					pk_hist.push_back(sig);
+					peaks.push_back(max(pk_hist));
+					// threshold_ptr.reset(&pool.getMedian(peaks));
+					threshold_ptr.reset(sig_median(peaks));
+				}else{
+					for(std::size_t j = 0; j < median_len; j++){
+						peaks.push_back(sig);
+					}
+					for(std::size_t j = 0; j < ping_width_samp; j++){
+						pk_hist.push_back(sig);
+					}
+					for(std::size_t j = 0; j < data_len; j++){
+						data.push_back(sig);
+					}
+
+					threshold_ptr.swap(sig);
+				}
+
+				signal_idx++;
+				id_signal.push_back(compare(sig, threshold_ptr, MIN_SNR));
+
+				// std::cout << "Fail after falling edge" << std::endl;
+				std::shared_ptr<std::set<std::size_t>> edge_ptr = has_falling_edge(id_signal.end() - 1);
+				if(!edge_ptr->empty()){
+					std::set<std::size_t>& edges = *edge_ptr;
+					for(auto it = edges.begin(); it != edges.end(); it++){
+						// std::cout << "Fail after pulse width" << std::endl;
+						const std::size_t pulse_width = get_pulse_width(id_signal.end() - 1, id_signal.begin(), *it);
+						auto ping_start = data.end() - 1 - pulse_width;
+						auto ping_end = data.end() - 1;
+						std::size_t ping_start_ms = (std::size_t)((signal_idx - 
+							pulse_width) * _ms_per_sample);
+						if(pulse_width < LOW_THRESHOLD * ping_width_samp){
+							continue;
+						}
+						if(pulse_width > HIGH_THRESHOLD * ping_width_samp){
+							continue;
+						}
+
+						// std::cout << "Fail after pulse magnitude" << std::endl;
+						const double amplitude = get_pulse_magnitude(ping_start, ping_end, *it);
+
+						PingPtr ping = std::make_shared<Ping>(ping_start_ms, amplitude, idxToFreq(*it));
+
+						{
+							std::unique_lock<std::mutex> out_lock(o_m);
+							o_q.push(ping);
+							out_count++;
+							out_lock.unlock();
+							o_v.notify_all();
+							std::cout << "Got ping!" << std::endl;
+						}
+					}
+				}
+			}
+		}
 	}
 
 	void DSP_V3::_unpack(std::queue<std::complex<double>*>& i_q, std::mutex& i_m, 
@@ -84,43 +330,30 @@ namespace RTT{
 		// Local vars
 		std::vector<std::complex<double>> double_data{};
 		
-		std::size_t file_counter = 0;
+		std::size_t file_counter = 1;
 		std::size_t sample_counter = 0;
+		std::size_t frame_counter = 0;
 		std::size_t integrate_counter = 0;
 		char fname[1024];
-		sprintf(fname, _output_fmt, file_counter + 1);
+		sprintf(fname, _output_fmt, file_counter);
 		std::ofstream data_str;
 
-		std::size_t FFT_LEN = 2048;
 
 		if(!_output_dir.empty()){
 			data_str.open(fname, std::ofstream::binary);
-			// std::cout << "Opening " << fname << std::endl;
+			std::cout << "Opening initial " << fname << std::endl;
 		}
 
-		fftw_complex* fft_in = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * FFT_LEN);
-		if(fft_in == nullptr){
-			std::cout << "Failed to allocate fft_in!" << std::endl;
-			return;
+		std::shared_ptr<std::vector<double>> integrator = std::make_shared<std::vector<double>>();
+		integrator->resize(nFreqs);
+		for(size_t i = 0; i < nFreqs; i++){
+			(*integrator)[i] = 0;
 		}
 
-
-		fftw_complex* fft_out = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * FFT_LEN);
-		if(fft_out == nullptr){
-			std::cout << "Failed to allocation fft_out!" << std::endl;
-			return;
-		}
-
-		if(!fftw_init_threads()){
-			std::cout << "Failed to initialize FFTW threads!" << std::endl;
-			return;
-		}
-		fftw_plan_with_nthreads(4);
-		fftw_plan fft_plan = fftw_plan_dft_1d(FFT_LEN, fft_in, fft_out,
-			FFTW_FORWARD, FFTW_MEASURE);
-
-		std::vector<double> integrator{};
-		integrator.resize(FFT_LEN);
+		int16_t* int_buf = new int16_t[2 * AbstractSDR::rx_buffer_size];
+		#ifdef DEBUG
+		std::ofstream _ostr1{"unpack_in.log"};
+		#endif
 
 		// Loop
 		while(_run || !i_q.empty()){
@@ -135,83 +368,68 @@ namespace RTT{
 				inputLock.unlock();
 
 				if(integrate_counter > int_factor){
+					// push integrator to queue
+					std::unique_lock<std::mutex> cLock(_c_m);
+					_c_q.push(integrator);
+					cLock.unlock();
+					_c_v.notify_all();
+					integrator.reset(new std::vector<double>());
+					integrator->resize(FFT_LEN);
 					for(size_t i = 0; i < FFT_LEN; i++){
-						integrator[i] = 0;
+						(*integrator)[i] = 0;
 					}
 					integrate_counter = 0;
 				}
 				integrate_counter += FFT_LEN;
 				sample_counter += FFT_LEN;
 
-				for(size_t i = 0; i < FFT_LEN; i++){
-					fft_in[i][0] = dataObj[i].real();
-					fft_in[i][1] = dataObj[i].imag();
+				#ifdef DEBUG
+				for(std::size_t i = 0; i < FFT_LEN; i++){
+					_ostr1 << dataObj[i].real();
+					if(dataObj[i].imag() >= 0){
+						_ostr1 << '+';
+					}
+					_ostr1 << dataObj[i].imag() << "i" << std::endl;
 				}
-				fftw_execute(fft_plan);
+				#endif
+
 				for(size_t i = 0; i < FFT_LEN; i++){
-					integrator[i] += std::abs(std::complex<double>(fft_out[i][0], fft_out[i][1])) * std::abs(std::complex<double>(fft_out[i][0], fft_out[i][1]));
+					_unpack_fft_in[i][0] = dataObj[i].real();
+					_unpack_fft_in[i][1] = dataObj[i].imag();
+				}
+				fftw_execute(_unpack_fft_plan);
+				for(size_t i = 0; i < nFreqs; i++){
+					(*integrator)[i] += std::abs(std::complex<double>(_unpack_fft_out[target_bins[i]][0], _unpack_fft_out[target_bins[i]][1])) * std::abs(std::complex<double>(_unpack_fft_out[target_bins[i]][0], _unpack_fft_out[target_bins[i]][1]));
 				}
 
-				// if(!_output_dir.empty()){
-				// 	for(std::size_t i = 0; i < dataObj->size(); i++){
-				// 		buffer[0] = (*(dataObj->data))[i].real();
-				// 		buffer[1] = (*(dataObj->data))[i].imag();
-				// 		data_str.write((char*)buffer, 2*sizeof(int16_t));
-				// 		sample_counter++;
-
-				// 		if(sample_counter >= SAMPLES_PER_FILE){
-				// 			data_str.close();
-				// 			file_counter++;
-				// 			sprintf(fname, _output_fmt, file_counter + 1);
-				// 			// std::cout << "Opening " << fname << std::endl;
-				// 			data_str.open(fname, std::ofstream::binary);
-				// 			sample_counter = 0;
-				// 		}
-				// 	}
-				// 	data_str.flush();
-				// }
-
-				// // update time_start_ms
-				// if(time_start_ms == 0){
-				// 	// hasn't been set
-				// 	time_start_ms = dataObj->time_ms;
-				// 	// TODO: update classifier!
-				// 	_clfr.setStartTime(dataObj->time_ms);
-				// }
-
-				// std::unique_lock<std::mutex> dataLock(_mag_mux);
-				// for(std::size_t j = 0; j < double_data.size(); j++){
-				// 	double data = std::abs(double_data[j]) * std::abs(double_data[j]);
-				// 	auto sig = new std::vector<std::complex<double>>();
-				// 	sig->push_back(double_data[j]);
-				// 	auto tsig = new TaggedSignal(data, *sig);
-				// 	_mag_data_queue.push(tsig);
-				// }
-				// dataLock.unlock();
-				// _mag_cv.notify_all();
+				
+				if(!_output_dir.empty()){
+					for(std::size_t i = 0; i < AbstractSDR::rx_buffer_size; i++){
+						int_buf[2*i] = dataObj[i].real();
+						int_buf[2*i + 1] = dataObj[i].imag();
+					}
+					data_str.write((char*)int_buf, AbstractSDR::rx_buffer_size * 2 * sizeof(int16_t));
+					data_str.flush();
+					if(frame_counter++ == FRAMES_PER_FILE){
+						data_str.close();
+						sprintf(fname, _output_fmt, ++file_counter);
+						std::cout << "Opening " << fname << std::endl;
+						data_str.open(fname, std::ofstream::binary);
+						frame_counter = 0;
+					}
+				}
 				delete[] dataObj;
 			}
 
-			// if(!_output_dir.empty()){
-			// 	data_str.write((char*)raw_buffer, rx_buffer_size * 2 * sizeof(int16_t));
-			// 	data_str.flush();
-			// 	if(frame_counter++ == FRAMES_PER_FILE){
-			// 		data_str.close();
-			// 		sprintf(fname, _output_fmt, file_counter++);
-			// 		std::cout << "Opening " << fname << std::endl;
-			// 		data_str.open(fname, std::ofstream::binary);
-			// 		frame_counter = 0;
-			// 	}
-			// }
 		}
 		if(!_output_dir.empty()){
 			data_str.close();
 		}
+		delete[] int_buf;
 	}
 
 	void DSP_V3::setStartTime(std::size_t start_time_ms){
 		time_start_ms = start_time_ms;
-		_clfr.setStartTime(time_start_ms);
 	}
 
 	void DSP_V3::setOutputDir(const std::string dir, const std::string fmt){
--- src/sdr_record/dspv3.hpp
+++ src/sdr_record/dspv3.hpp
@@ -8,12 +8,16 @@
 #include <condition_variable>
 #include <thread>
 #include "tagged_signal.hpp"
+#include <fftw3.h>
+#include <memory>
+#include <set>
+#include "AbstractSDR.hpp"
 
 namespace RTT{
 	class DSP_V3 : public DSP{
 
 	public:
-		DSP_V3(const std::size_t sampling_freq, const std::size_t center_freq);
+		DSP_V3(const std::size_t sampling_freq, const std::size_t center_freq, const std::vector<std::size_t>& target_freqs);
 		~DSP_V3();
 		void startProcessing(std::queue<std::complex<double>*>& inputQueue,
 			std::mutex& inputMutex, std::condition_variable& inputVar,
@@ -33,6 +37,11 @@ namespace RTT{
 		 */
 		void _unpack(std::queue<std::complex<double>*>& i_q, std::mutex& i_m, 
 			std::condition_variable& i_v);
+
+
+		void classify(std::queue<std::shared_ptr<std::vector<double>>>&, std::mutex&,
+			std::condition_variable&, std::queue<PingPtr>&, std::mutex&,
+			std::condition_variable&);
 		std::condition_variable* _in_v;
 		std::thread* _thread;
 		volatile bool _run = false;
@@ -42,41 +51,67 @@ namespace RTT{
 		 */
 		std::size_t time_start_ms = 0;
 
-		/**
-		 * IQ data queue from unpack to FIR
-		 */
-		std::queue<std::complex<double>> _iq_data_queue;
-		std::mutex _iq_mux;
-		std::condition_variable _iq_cv;
 
-		FIR _fir;
+		std::queue<std::shared_ptr<std::vector<double>>> _c_q;
+		std::mutex _c_m;
+		std::condition_variable _c_v;
+		std::thread* _c_thread;
 
-		/**
-		 * Magnitude Data queue from FIR to Integrator
-		 */
-		std::queue<TaggedSignal*> _mag_data_queue;
-		std::mutex _mag_mux;
-		std::condition_variable _mag_cv;
-
-		const float int_time_s = 3.2e-3;
-		std::size_t int_factor = 800;
-
-		Integrator _int;
-
-		/**
-		 * Candidate signal data queue from Integrator to Classifier
-		 */
-		std::queue<TaggedSignal*> _candidate_queue;
-		std::mutex _can_mux;
-		std::condition_variable _can_cv;
-
-		Classifier _clfr;
+		const float int_time_s = 3e-3;
+		std::size_t int_factor;
 
 		std::string _output_dir;
 		char* _output_fmt;
 
-		const std::size_t SAMPLES_PER_FILE = 64*1024*1024/sizeof(int16_t) / 2;
+		std::size_t maximizer_len;
+		std::size_t median_len;
+		std::size_t data_len;
+		
+		// 2017 data
+		const static std::size_t ping_width_ms = 15;
+		const double MIN_SNR = 0.07;
+		const double HIGH_THRESHOLD = 2;
+		const double LOW_THRESHOLD = 0.75;
 
+		// 2019 test data
+		// const static std::size_t ping_width_ms = 22;
+		// const double MIN_SNR = 0.4;
+		// const double HIGH_THRESHOLD = 1.5;
+		// const double LOW_THRESHOLD = 0.75;std::size_t ping_width_samp;
+		std::size_t ping_width_samp;
+		const std::size_t FFT_LEN = 2048;
+		double clfr_input_freq;
+
+
+		const std::size_t SAMPLES_PER_FILE = 64*1024*1024/sizeof(int16_t) / 2;
+		const std::size_t FRAMES_PER_FILE = SAMPLES_PER_FILE / AbstractSDR::rx_buffer_size;
+
+		std::shared_ptr<std::vector<double>> max(boost::circular_buffer<std::shared_ptr<std::vector<double>>>& sig);
+
+		std::vector<double>* sig_median(boost::circular_buffer<std::shared_ptr<std::vector<double>>>& sig);
+
+		fftw_complex* _unpack_fft_in;
+		fftw_complex* _unpack_fft_out;
+		fftw_plan _unpack_fft_plan;
+
+		std::shared_ptr<bool> compare(std::shared_ptr<std::vector<double>> data, std::shared_ptr<std::vector<double>> threshold, double min_snr);
+		std::shared_ptr<std::set<std::size_t>> has_falling_edge(boost::circular_buffer<std::shared_ptr<bool>>::iterator it);
+
+		const std::size_t get_pulse_width(
+		boost::circular_buffer<std::shared_ptr<bool>>::iterator begin,
+		boost::circular_buffer<std::shared_ptr<bool>>::iterator end,
+		std::size_t i);
+		double _ms_per_sample;
+
+		const std::vector<std::size_t> target_freqs;
+		const std::size_t nFreqs;
+		std::vector<std::size_t> target_bins;
+		const double pow(const fftw_complex* sample) const;
+		const double get_pulse_magnitude(boost::circular_buffer<std::shared_ptr<std::vector<double>>>::iterator start,
+			boost::circular_buffer<std::shared_ptr<std::vector<double>>>::iterator end, std::size_t idx) const;
+		const std::size_t idxToFreq(const std::size_t idx) const;
+		const std::size_t s_freq;
+		const std::size_t c_freq;
 	};
 }
 #endif
