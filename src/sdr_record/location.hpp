a8aab9c17f23aaa12ad8314ec268ec0460254c27
Nathan Hui <ntlhui@gmail.com>
2019-07-17 19:43:24 -0700

    Added default values

--- src/sdr_record/location.hpp
+++ src/sdr_record/location.hpp
@@ -12,52 +12,52 @@ namespace RTT{
 		/**
 		 * Local system time in ms since Unix Epoch
 		 */
-		std::uint64_t ltime;
+		std::uint64_t ltime = 0;
 
 		/**
 		 * Global time in ms since GPS Epoch
 		 */
-		std::uint64_t gtime;
+		std::uint64_t gtime = 0;
 
 		/**
 		 * degrees*1e7 latitude from the WGS84 datum
 		 */
-		std::int64_t lat;
+		std::int64_t lat = 0;
 
 		/**
 		 * degrees*1e7 longitude from the WGS84 datum
 		 */
-		std::int64_t lon;
+		std::int64_t lon = 0;
 
 		/**
 		 * Altitude in meters from WGS84 spheroid
 		 */
-		std::int32_t alt;
+		std::int32_t alt = 0;
 
 		/**
 		 * Altitude in meters from the starting altitude
 		 */
-		std::int32_t rel_alt;
+		std::int32_t rel_alt = 0;
 
 		/**
 		 * Speed in the local x axis in m/s
 		 */
-		float vx;
+		float vx = 0;
 
 		/**
 		 * Speed in the local y axis in m/s
 		 */
-		float vy;
+		float vy = 0;
 
 		/**
 		 * Speed in the local z axis in m/s
 		 */
-		float vz;
+		float vz = 0;
 
 		/**
 		 * Heading in degrees from True North in WGS84
 		 */
-		std::int16_t hdg;
+		std::int16_t hdg = 0;
 	};
 }
 
