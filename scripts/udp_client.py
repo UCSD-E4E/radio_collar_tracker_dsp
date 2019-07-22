a85743b86cd5c8f713029a3c5926b7e0916d51b9
Nathan Hui <ntlhui@gmail.com>
2019-07-22 11:57:21 -0700

    Added hearbeat

--- scripts/udp_client.py
+++ scripts/udp_client.py
@@ -4,6 +4,8 @@ import socket
 import argparse
 import os
 import json
+import datetime
+import time
 
 def main():
 	parser = argparse.ArgumentParser("UDP Client")
@@ -18,20 +20,30 @@ def main():
 	localization_file = open(os.path.join(data_dir, 'LOCALIZE_%06d' % (run_num)))
 
 	UDP_PORT = 9000
+	UDP_IP = '255.255.255.255'
 
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+	prevTime = datetime.datetime.now()
 
 	try:
 		while True:
+			now = datetime.datetime.now()
+			if (now - prevTime).total_seconds() > 1:
+				msg = "{heartbeat:{time: %d}}\n" % (time.mktime(now.timetuple()))
+				print msg
+				sock.sendto(msg, (UDP_IP, UDP_PORT))
+				prevTime = now
 			line = localization_file.readline()
 			if line == '':
 				continue
 			if 'stop' in json.loads(line):
 				break
-			sock.sendto(line.encode('utf-8'), ('255.255.255.255', UDP_PORT))
+			print line
+			sock.sendto(line.encode('utf-8'), (UDP_IP, UDP_PORT))
 	except:
+		print("Early Fail!")
 		pass
 	localization_file.close()
 	sock.close()
