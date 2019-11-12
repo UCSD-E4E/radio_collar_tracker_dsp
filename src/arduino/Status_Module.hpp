f3636ee34072ba0e00e252ea85c64588e9f2853d
Nathan Hui <ntlhui@gmail.com>
2019-11-12 15:53:03 -0800

    Added docstrings

--- src/arduino/Status_Module.hpp
+++ src/arduino/Status_Module.hpp
@@ -3,17 +3,43 @@
 
 #include "Status_Packet.hpp"
 
+/**
+ * Status Module for interpreting status information from the OBC.
+ */
 class Status_Module{
 public:
-
-	
-
+	/**
+	 * Returns a const reference to the status packet representing the current
+	 * state of all RTT systems.
+	 * @return const reference to a StatusPacket
+	 */
 	const StatusPacket& getStatus() const;
 
-
+	/**
+	 * Default constructor.  This initializes the internal state machines. This
+	 * decoder is ready to operate immediately after this constructor.  This
+	 * constructor will use an internally owned StatusPacket instance.
+	 */
 	Status_Module();
+
+	/**
+	 * Alternate constructor to use a specific StatusPacket instance.  This
+	 * decoder is ready to operate immediately after this constructor.
+	 */
 	Status_Module(StatusPacket* packet);
+
+	/**
+	 * Destructor.
+	 */
 	~Status_Module();
+
+	/**
+	 * Decodes each status packet per character - feed each character of the
+	 * stream into this function to decode the entire message.  This function
+	 * will return 1 iff a full message has been received, 0 otherwise.
+	 * @param  c Next character in the status packet.
+	 * @return   1 if a full message has been received, 0 otherwise.
+	 */
 	int decode(char c);
 private:
 	enum ParserState{
