7650b8031f0833e0c6500bff981a52e5a180d98a
Nathan Hui <ntlhui@gmail.com>
2019-11-19 16:04:26 -0800

    Added docstring to RTT::PingLocalizer

--- src/sdr_record/localization.hpp
+++ src/sdr_record/localization.hpp
@@ -12,40 +12,77 @@
 
 namespace RTT{
 	
-
+	/**
+	 * The PingLocalizer is responsible for localizing pings and writing the 
+	 * data to a file, that udp_command.CommandListener then forwards to the 
+	 * ground station.
+	 */
 	class PingLocalizer{
 
 	public:
-		// typedef dlib::matrix<double, 5, 1> parameter_vector;
-		// typedef dlib::matrix<double, 3, 1> input_vector;
+		/**
+		 * Creates the PingLocalizer that writes to the specified output file.
+		 * @param out Output file
+		 */
 		PingLocalizer(std::ostream& out);
+
+		/**
+		 * Destructor
+		 */
 		~PingLocalizer();
-		void start(std::queue<PingPtr>&, std::mutex&, std::condition_variable&, 
-			GPS&);
+
+		/**
+		 * Starts the PingLocalizer threads with the specified input queue for
+		 * pings and GPS module.
+		 * @param i_q		Input queue of PingPtr objects
+		 * @param i_m		Input queue mutex
+		 * @param i_v		Input queue condition variable
+		 * @param module	GPS Module to retrieve localization information from
+		 */
+		void start(std::queue<PingPtr>& i_q, std::mutex& i_m, 
+			std::condition_variable& i_v, GPS& module);
+
+		/**
+		 * Informs the PingLocalizer module to complete processing of queued
+		 * pings and exit.
+		 */
 		void stop();
 
-		double getLat();
-		double getLon();
-		double getK1();
-		double getK2();
 	protected:
 	private:
+		/**
+		 * Localizes the input pings using data from the gps_module
+		 * @param queue      Input queue
+		 * @param mutex      Input queue mutex
+		 * @param var        Input queue condition variable
+		 * @param gps_module GPS data source
+		 */
 		void process(std::queue<PingPtr>& queue, std::mutex& mutex,
-			std::condition_variable&, GPS& gps_module);
+			std::condition_variable& var, GPS& gps_module);
+
+		/**
+		 * Internal state variable
+		 */
 		volatile bool run;
+
+		/**
+		 * Thread pointer for process
+		 */
 		std::thread* localizer_thread;
+
+		/**
+		 * handle to condition variable to wake thread
+		 */
 		std::condition_variable* _input_cv;
-		// parameter_vector params;
-		// struct estimate_result{
-		// 	parameter_vector params;
-		// 	double mse;
-		// };
 
-		// estimate_result _estimate;
-
-		// estimate_result& estimate(std::vector<std::pair<input_vector, double>> data);
-
+		/**
+		 * Output filestream
+		 */
 		std::ostream& _out;
+
+		/**
+		 * Secondary queue for recycling pings that arrived before GPS fix.
+		 */
 		std::queue<PingPtr> pingQueue;
 	};
 }
